923f8f36a26bc99263ed319b12cbf1b4a44754d3
Vandan Rohatgi <vandanrohatgi@gmail.com>
2023-12-04 12:04:59 +0530

    chore: add omitempty tags

--- main.go
+++ main.go
@@ -5,26 +5,27 @@ import (
 )
 
 const (
-	kevURL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
+	kevFeedURL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
+	kevCSVURL  = "https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv"
 )
 
 type Catalogue struct {
-	Title           string          `json:"title"`
-	CatalogVersion  string          `json:"catalogVersion"`
-	DateReleased    time.Time       `json:"dateReleased"`
-	Count           int             `json:"count"`
-	Vulnerabilities []Vulnerability `json:"vulnerabilities"`
+	Title           string            `json:"title,omitempty"`
+	CatalogVersion  string            `json:"catalogVersion,omitempty"`
+	DateReleased    time.Time         `json:"dateReleased,omitempty"`
+	Count           int               `json:"count,omitempty"`
+	Vulnerabilities []Vulnerabilities `json:"vulnerabilities,omitempty"`
 }
 
-type Vulnerability struct {
-	CveID                      string `json:"cveID"`
-	VendorProject              string `json:"vendorProject"`
-	Product                    string `json:"product"`
-	VulnerabilityName          string `json:"vulnerabilityName"`
-	DateAdded                  string `json:"dateAdded"`
-	ShortDescription           string `json:"shortDescription"`
-	RequiredAction             string `json:"requiredAction"`
-	DueDate                    string `json:"dueDate"`
-	KnownRansomwareCampaignUse string `json:"knownRansomwareCampaignUse"`
-	Notes                      string `json:"notes"`
+type Vulnerabilities struct {
+	CveID                      string `json:"cveID,omitempty"`
+	VendorProject              string `json:"vendorProject,omitempty"`
+	Product                    string `json:"product,omitempty"`
+	VulnerabilityName          string `json:"vulnerabilityName,omitempty"`
+	DateAdded                  string `json:"dateAdded,omitempty"`
+	ShortDescription           string `json:"shortDescription,omitempty"`
+	RequiredAction             string `json:"requiredAction,omitempty"`
+	DueDate                    string `json:"dueDate,omitempty"`
+	KnownRansomwareCampaignUse string `json:"knownRansomwareCampaignUse,omitempty"`
+	Notes                      string `json:"notes,omitempty"`
 }
